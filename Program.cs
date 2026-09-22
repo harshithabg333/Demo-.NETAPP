448142642c65dcbe6b86f4216978bf9d82ca203f
Harshitha BG <harshithabg.19@gmail.com>
2026-09-22 19:36:12 +0530

    Update Program.cs
    changed month to 9

--- Program.cs
+++ Program.cs
@@ -2,5 +2,5 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 var app = builder.Build();
 app.MapControllers();
-app.MapGet("/", () => "Change in the configuration file 22-8-2027");
+app.MapGet("/", () => "Change in the configuration file 22-9-2027");
 app.Run();
